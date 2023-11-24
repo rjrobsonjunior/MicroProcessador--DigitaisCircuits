c855f89b44b940c0febb5907795c6a260f9f406f
Robson Junior <105251336+rjrobsonjunior@users.noreply.github.com>
2023-11-24 10:57:24 -0300

    bugfix FullAdder

--- MicroProcessador.vhd
+++ MicroProcessador.vhd
@@ -1,11 +1,34 @@
-library ieee;
-use iee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
 
-entity MicroProcessador is 
-    port(
-    clk, PB0 : in std_logic;
-    Display : out std_logic_vector ( 7 downto 0);
-    );
+entity MicroProcessador is
+	port(
+		clk: in std_logic;
+      A, B: in std_logic_vector(7 downto 0);
+      CTRL: in std_logic;
+      Couts: out std_logic_vector(7 downto 0);
+      Msig: out std_logic
+	);
 end MicroProcessador;
 
+architecture MicroProcessador_arch of MicroProcessador is
+	
+	component FullAdder8bits is
+		port(	
+			Eu: in std_logic;
+			A, B: in std_logic_vector(7 downto 0);
+			Su: in std_logic;
+			Result: out std_logic_vector(7 downto 0);
+			Cout: out std_logic
+		);
+		end component;
+--COMPONENT--END--
+
+
+--BEGIN--
+	begin
+	
+	contador: FullAdder8bits port map(  clk, A, B, CTRL, Couts, Msig );
+	
+end MicroProcessador_arch;
